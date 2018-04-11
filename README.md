2af5f4f47a5252434071c5f996d2d83d59074271
maurolepore <maurolepore@gmail.com>
2018-04-11 22:32:23 +0000

    Create template

---
+++ README.md
@@ -0,0 +1,10 @@
+talks-template
+================
+
+Model of a presentation repo. Inspired by [this repo](https://github.com/jennybc/row-oriented-workflows#readme) Jenny Bryan.
+
+-   [index.Rmd](index.Rmd): Master file that gathers all children code examples and creates a presentation. The name index is for GitHub pages to build a webpage.
+
+-   [ex01\_knit-md.Rmd](ex01_knit-md.md): How to knit each code example to .md.
+
+-   [ex02\_purl-r.Rmd](ex02_purl-r.Rmd): How to purl each code example to .R.
---
+++ ex02_purl-r.R
@@ -0,0 +1,32 @@
+#' ---
+#' title: "Code example 2"
+#' output: github_document
+#' ---
+#' 
+#' 
+#' 
+#' # File 2
+#' 
+#' This file is a distinct code example.
+#' 
+#' ## `purl()` to produce .R files
+#' 
+#' .R versions of the files may be good for students to follow your slides.
+#' 
+#' ```
+#' files <- dir()[grepl("^ex", dir())]
+#' lapply(files, knitr::purl, documentation = 2)
+#' ```
+#' 
+#' ## Test
+#' 
+#' Expect render.
+#' 
+#' 
+#' ```r
+#' 1 + 1
+#' ```
+#' 
+#' ```
+#' ## [1] 2
+#' ```
